e8b694ff5af445f210b9f83c69ceaac4c74ef525
priyanthi44 <pree4ever@gmail.com>
2020-08-21 09:03:56 +0100

    Theme

---
+++ app/src/main/java/com/codingwithmitch/notes/models/Note.java
@@ -0,0 +1,116 @@
+package com.codingwithmitch.notes.models;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+@Entity(tableName = "notes")
+public class Note implements Parcelable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "content")
+    private String content;
+
+    @ColumnInfo(name = "timestamp")
+    private String timestamp;
+
+    @ColumnInfo(name = "quantity")
+    private int quantity;
+
+    public Note(String title, String content, String timestamp, int quantity) {
+        this.title = title;
+        this.content = content;
+        this.timestamp = timestamp;
+        this.quantity = quantity;
+    }
+
+    @Ignore
+    public Note() {
+
+    }
+
+
+    protected Note(Parcel in) {
+        id = in.readInt();
+        title = in.readString();
+        content = in.readString();
+        timestamp = in.readString();
+        quantity = in.readInt();
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", timestamp='" + timestamp + '\'' +
+                ", quantity='" + quantity + '\'' +
+                '}';
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(id);
+        parcel.writeString(title);
+        parcel.writeString(content);
+        parcel.writeString(timestamp);
+    }
+}
